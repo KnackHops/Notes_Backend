507c49e86fa1a0989b8721c823dcac55a62047b0
Aldrian <himalosc@gmail.com>
2021-07-21 22:06:44 +0800

    configured url for db, and db itself

--- flaskr/user.py
+++ flaskr/user.py
@@ -1,5 +1,22 @@
+import click
 from flask import (
-    Blueprint
+    Blueprint, request
 )
 
-bp = Blueprint("user", __name__, "/user")
+from flaskr.db import (
+    get_db,
+)
+
+bp = Blueprint('user', __name__, url_prefix='/user')
+
+
+@bp.route('/register')
+def register():
+    db = None
+
+    # if request.method == 'POST':
+    #     db = get_db('login')
+
+    db = get_db('login')
+
+    return f'<p>yip<p>'
