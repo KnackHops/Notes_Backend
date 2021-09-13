a161caf09051cfd6ed4ea1e1d84b8353f50522ea
Aldrian <himalosc@gmail.com>
2021-09-14 00:46:36 +0800

    fixing given url being incompatible

--- flaskr/config.py
+++ flaskr/config.py
@@ -3,4 +3,4 @@ import os
 
 class Config(object):
     Testing = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('://', 'ql+psycopg2://')
