d21b95b29b2930746bb1052200f578f1f53858d3
Aldrian <himalosc@gmail.com>
2021-09-13 21:43:16 +0800

    database url

--- flaskr/config.py
+++ flaskr/config.py
@@ -3,6 +3,4 @@ import os
 
 class Config(object):
     Testing = False
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://postgres:" \
-                              f"{os.environ.password_key}@" \
-                              f"{os.environ.get('DATABASE_URL')}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://@{os.environ.get('DATABASE_URL')}"
