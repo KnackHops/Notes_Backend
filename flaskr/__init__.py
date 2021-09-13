e1cb4837c1ace524c8c3ab1df0d65f7013c97bab
Aldrian <himalosc@gmail.com>
2021-09-14 01:09:00 +0800

    app fully deployed

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,6 +11,7 @@ from flask_cors import (
 )
 from flask_sqlalchemy import SQLAlchemy
 
+_app = None
 _sq = None
 _User = None
 _Login = None
@@ -97,11 +98,11 @@ def create_app():
     # app.cli.add_command(hello_world)
     # app.cli.add_command(up_no_note)
     # app.cli.add_command(dbase_check)
-
+    global _app
+    _app = app
     return app
 
 if __name__ == '__main__':
-    from flaskr import app
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    _app.run(host='0.0.0.0', port=port)
